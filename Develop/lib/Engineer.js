ce49e7cd77198b7ff0be2e13b37ce7c64f39bb93
jdab2020 <jdabalos90@gmail.com>
2020-02-19 20:07:56 -0800

    added classes

---
+++ Develop/lib/Engineer.js
@@ -0,0 +1,16 @@
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(github) {
+        super(name,id,title);
+        this.github = github;
+    }
+
+    getGithub() {
+
+    }
+
+    getRole() {
+        
+    }
+}
